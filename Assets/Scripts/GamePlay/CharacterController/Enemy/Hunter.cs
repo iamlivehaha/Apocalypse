95df21b75b92fd71df762f804966449409535a2a
DESKTOP-27KCDVJ\Jett <879273541@qq.com>
2020-10-26 22:27:02 +0800

    -Add attack Trigger

--- Assets/Scripts/GamePlay/CharacterController/Enemy/Hunter.cs
+++ Assets/Scripts/GamePlay/CharacterController/Enemy/Hunter.cs
@@ -38,7 +38,7 @@ namespace Assets.Scripts.GamePlay.CharacterController.Enemy
 
             // property setting
             m_animator.SetBool("bpatrol", bPatrol);
-            mattackTempInterval = m_attackInterval-1;
+            mattackTempInterval = m_attackInterval - 1;
             if (m_defaultDir == DefaultDirection.Right)
             {
                 Attackoffset = Attackoffset_right;
@@ -80,21 +80,16 @@ namespace Assets.Scripts.GamePlay.CharacterController.Enemy
 
         private void LookTarget(Vector3 mTargetPos)
         {
-            Vector3 attackDir = (mTargetPos+Vector3.up - transform.position).normalized;
+            Vector3 attackDir = (mTargetPos + Vector3.up - transform.position).normalized;
             float angle = Vector3.Angle(new Vector3(attackDir.x > 0 ? 1 : -1, 0, 0), attackDir);
             float dir = (mTargetPos - transform.position).normalized.x;
             lookRot = Quaternion.AngleAxis(Attackoffset - angle, Vector3.forward);
-            m_weapon.transform.rotation = Quaternion.Slerp(m_weapon.transform.rotation, lookRot, Time.deltaTime*2);
+            m_weapon.transform.rotation = Quaternion.Slerp(m_weapon.transform.rotation, lookRot, Time.deltaTime * 2);
             //m_weapon.transform.rotation = rot;
         }
         IEnumerator StartAttack(Transform mTarget)
         {
-            //shootlookRot = Quaternion.AngleAxis(180 - lookRot.eulerAngles.z, Vector3.forward);
-            //if (!isAttack)
-            //{
-            //    bool isleft = !(m_target.transform.position.x - transform.position.x > 0);
-            //    m_weapon.Attack(m_target.gameObject, isleft ? lookRot : shootlookRot);
-            //}
+
             while (true)
             {
                 if (!bTargetInView)
@@ -108,7 +103,7 @@ namespace Assets.Scripts.GamePlay.CharacterController.Enemy
                 {
                     m_animator.SetTrigger("attack");
                     //todo arrow attack 
-                    shootlookRot = Quaternion.AngleAxis(180-lookRot.eulerAngles.z,Vector3.forward);
+                    shootlookRot = Quaternion.AngleAxis(180 - lookRot.eulerAngles.z, Vector3.forward);
                     yield return new WaitForSeconds(m_attackInterval);
                 }
                 else
@@ -118,6 +113,14 @@ namespace Assets.Scripts.GamePlay.CharacterController.Enemy
                 }
 
             }
+        }
+
+        public void AttackImmediately()
+        {
+            shootlookRot = Quaternion.AngleAxis(180 - lookRot.eulerAngles.z, Vector3.forward);
+            bool isleft = !(m_target.transform.position.x - transform.position.x > 0);
+            m_weapon.Attack(m_target.gameObject, isleft ? lookRot : shootlookRot);
+
         }
         public override void Attack(GameObject o)
         {
---
+++ Assets/Scripts/GamePlay/Interactive object/AttackTrigger.cs
@@ -0,0 +1,39 @@
+﻿using Assets.Scripts.GamePlay.CharacterController.Enemy;
+using UnityEngine;
+
+namespace Assets.Scripts.GamePlay.Interactive_object
+{
+    public class AttackTrigger : MonoBehaviour
+    {
+        public GameObject m_attacker;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+        public void OnTriggerEnter(Collider col)
+        {
+            //spawn point check
+            if (col.transform.tag == "Player")
+            {
+                m_attacker.GetComponent<Hunter>().AttackImmediately();
+            }
+        }
+        public void OnTriggerExit(Collider col)
+        {
+            //spawn point check
+            //if (col.transform.tag == "Player")
+            //{
+            //m_attacker.GetComponent<Hunter>().AttackImmediately();
+            //}
+        }
+    }
+}
+
